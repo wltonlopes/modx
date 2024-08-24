20d219b018d3f765edd3a0f34d3daec9aa6001c5
Welton Lopes <39802614+wltonlopes@users.noreply.github.com>
2024-08-24 16:18:17 -0300

    ahah

--- gui/pregame/backgrounds/space.js
+++ gui/pregame/backgrounds/space.js
@@ -1,8 +1,8 @@
 g_BackgroundLayerData.push(
 	[
 		{
-			"offset": (time, width) => 0.0 * width * Math.cos(0.0 * time),
-			"sprite": "space",
+			"offset": (time, width) => 0.16 * width * Math.cos(0.08 * time) + width/16,
+			"sprite": "background-caesar1-1",
 			"tiling": false,
-		},
+		}
 	]);
