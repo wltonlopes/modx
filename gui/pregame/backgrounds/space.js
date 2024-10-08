9c0254cce3defa167d007ea7ee5bc61f7014b1f8
Welton Lopes <39802614+wltonlopes@users.noreply.github.com>
2024-10-08 08:34:16 -0300

    atualizações

--- gui/pregame/backgrounds/space.js
+++ gui/pregame/backgrounds/space.js
@@ -1,8 +1,8 @@
 g_BackgroundLayerData.push(
 	[
 		{
-			"offset": (time, width) => 0.16 * width * Math.cos(0.08 * time) + width/16,
-			"sprite": "background-caesar1-1",
+			"offset": (time, width) => 0.02 * width * Math.cos(0.08 * time) + width/16,
+			"sprite": "background-b2",
 			"tiling": false,
 		}
 	]);
